a3159c5d81d6d3daa2800d59b52533e1e6804241
Walter Ocampo <fearless347@gmail.com>
2021-12-09 15:15:18 -0500

    challenge 09 done

---
+++ challenge-09.js
@@ -0,0 +1,30 @@
+export default function groupBy(itemsInFactory = [], criteriaToEvaluate) {
+  const objectResult = {};
+  const getValue = arg => arg[criteriaToEvaluate] || criteriaToEvaluate(arg);
+  itemsInFactory.forEach(item => {
+    objectResult[getValue(item)] = null;
+  });
+  Object.keys(objectResult).forEach(key => {
+    objectResult[key] = itemsInFactory.filter(element => {
+      return key === getValue(element).toString() && element;
+    });
+  });
+  return objectResult;
+}
+
+groupBy([6.1, 4.2, 6.3], Math.floor) // { 6: [6.1, 6.3], 4: [4.2] }
+groupBy(['one', 'two', 'three'], 'length') // { 3: ['one', 'two'], 5: ['three'] }
+groupBy([{ age: 23 }, { age: 24 }], 'age') // { 23: [{age: 23}], 24: [{age: 24}] }
+groupBy(
+  [1397639141184, 1363223700000],
+  timestamp => new Date(timestamp).getFullYear()
+) // { 2013: [1363223700000], 2014: [1397639141184] }
+
+groupBy([
+  { title: 'JavaScript: The Good Parts', rating: 8 },
+  { title: 'Aprendiendo Git', rating: 10 },
+  { title: 'Clean Code', rating: 9 },
+], 'rating')
+// { 8: [{ title: 'JavaScript: The Good Parts', rating: 8 }],
+// 9: [{ title: 'Clean Code', rating: 9 }],
+// 10: [{ title: 'Aprendiendo Git', rating: 10 }] }
